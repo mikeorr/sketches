319a61888feae84a21e313f5049e54c81e2a33e6
Mike Orr <sluggoster@gmail.com>
2021-02-16 16:16:14 -0800

    Reverse order of cards in testing display.

--- spysol/02_svelte/src/Spysol.svelte
+++ spysol/02_svelte/src/Spysol.svelte
@@ -4,6 +4,7 @@
 
   let deck = new models.Deck()
   let sorted = deck.sorted();
+  sorted.reverse()
 
   svelte.onMount(() => {
   });
@@ -18,8 +19,6 @@
   }
   .card {
     font-family: monospace;
-    /* font-family: serif; */
-    /* font-family: sans-serif; */
   }
   .card-black {
     color: black;
@@ -29,6 +28,7 @@
   }
   .card-name {
     font-size: 20px;
+    border: solid 1px;
   }
   .card-chr {
     font-size: 40px;
@@ -42,13 +42,11 @@
     <summary>Testing</summary>
       <h1>Sorted card names</h1>
       {#each sorted as card (card.id)}
-      <span class="card card-name" class:card-red={card.red} class:card-black={!card.red}>
-        {card.name.padStart(3)}&nbsp;
-      </span>
+      <span class="card card-name" class:card-red={card.red} class:card-black={!card.red}>{card.name.padStart(3)}</span>
       {/each}
       <h1>Sorted card chars</h1>
       {#each sorted as card (card.id)}
-      <span class="card card-chr {card.red ? 'card-red' : 'card-black'}" style="font-size: 40px">{card.chr}</span>
+      <span class="card card-chr {card.red ? 'card-red' : 'card-black'}">{card.chr}</span>
       {/each}
   </details>
 </div>
