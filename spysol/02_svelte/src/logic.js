114daecfe7a01ba4547001d02f8eb852a952ada9
Mike Orr <sluggoster@gmail.com>
2021-03-28 21:47:55 -0700

    Reduce card constructor back to just ID.

--- spysol/02_svelte/src/logic.js
+++ spysol/02_svelte/src/logic.js
@@ -3,47 +3,42 @@ import shuffleArray from "array-shuffle";
 const maxCards = 104;
 
 export class Card {
-    static _letterRanks = {1: "A", 11: "J", 12: "Q", 13: "K"};
-    constructor (id, rank, clubs, faceUp=true) {
-        const chrBase = clubs ? 0x1F0D1 : 0x1F0B1;
-        const chrOffset = rank > 11 ? (rank + 1) : rank;
+    static _rankLetters = {1: "A", 11: "J", 12: "Q", 13: "K"};
+
+    constructor (id, faceUp=false) {
+        const rs = (id - 1) % 26;    // 0-12 = clubs A-K, 13-25 = hearts A-K.
+        const clubs = rs < 13;       // true = clubs|black, false = hearts|red.
+        const rank = (rs % 13) + 1;  // rank 1-13.
         this.id = id;
         this.rank = rank;
-        this.suit = String.fromCodePoint(clubs ? 0x2663 : 0x2665);
-        this.black = clubs;
-        this.red = !clubs;
-        this.color = clubs ? "black" : "red";
-        this.name = (this.constructor._letterRanks[rank] || rank) + this.suit;
-        this.chr = this._getChr(rank, clubs);
         this.faceUp = faceUp;
+        if (clubs) {
+            this.suit = "\u{2663}";
+            this.color = "black";
+            this.black = true;
+        } else {
+            this.suit = "\u{2665}";
+            this.color = "red";
+            this.black = false;
+        }
+        this.name = (Card._rankLetters[rank] || rank) + this.suit;
+        this.chr = this._getChr(rank, clubs);
     }
 
     _getChr(rank, clubs) {
-        let base, ord;
-        base = clubs ? 0x1F0D1 : 0x1F0B1;
-        if (rank < 12) {
-            ord = base + rank - 1;  // Ace to Jack are zero offset from base.
-        } else {
-            ord = base + rank;   // Skip Cavalier between Jack and Queen.
-        }
-        return String.fromCodePoint(ord);
+        const base = clubs ? 0x1F0D1 : 0x1F0B1;
+        const offset = (rank < 12) ? (rank - 1) : rank;
+        return String.fromCodePoint(base + offset);
     }
 }
 
 
 export function makeCardDeck(faceUp=true, shuffled=false, order=null) {
-    const clubsArr = [true, false, true, false, true, false, true, false];
-    const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let rank;
-    let clubs;
-    let deck = [];
-    let id = -1;
-    let card;
-    for (clubs of clubsArr) {
-        for (rank of ranks) {
-            card = new Card(++id, rank, clubs, faceUp);
-            deck.push(card);
-        }
+    let card, deck, id;
+    deck = [];
+    for (id = 1; id <= 104; id++) {
+        card = new Card(id, faceUp);
+        deck.push(card);
     }
     if (order) {
         deck = order.map(i => deck[i]);
