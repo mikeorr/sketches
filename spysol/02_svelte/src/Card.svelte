247a17c6febf8ce2c124f1e44bf7a2e32c92b388
Mike Orr <sluggoster@gmail.com>
2021-08-23 14:00:10 -0700

    Unfinished.

--- spysol/02_svelte/src/Card.svelte
+++ spysol/02_svelte/src/Card.svelte
@@ -3,6 +3,96 @@
 // Properties.
 export let card;
 
+  // Properties.
+  export let card = {id: 0, rank: 1, suit: 0};
+  export let faceUp = true;
+  export let peek = false;
+  export let deck = "unicode";
+
+
+  // Other reactive variables.
+  let suitClass;
+  let text;
+
+  const patterns = {
+    letters: {
+        suitRanks: [
+            ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"],
+            ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"],
+        ],
+        back: "X",
+        align: "center",
+    },
+    cards: {
+        suitRanks: [
+            [0x1f0d1, 0x1f0d2, 0x1f0d3, 0x1fd4, 0x1fd5,
+             0x1fd6, 0x1f0d7, 0x1f0d8, 0x1f0d8, 0x1f0da,
+             0x1f0db, 0x1fdd, 0x1fde].map(String.fromCodePoint),
+            [0x1f0b1, 0x1f0b2, 0x1f0b3, 0x10fb4, 0x1fb5,
+             0x1fb6, 0x1f0b7, 0x1f0b8, 0x1f0b8, 0x1f0ba,
+             0x1f0bb, 0x1fbd, 0x1fbe].map(String.fromCodePoint),
+        ],
+        back: String.fromCodePoint(0x1f0a0),
+        align: "right".
+    },
+    ;
+  }
+
+  $: if (card.faceUp) {
+    suitClass = card.clubs ? "clubs" : "hearts";
+  } else if (peek) {
+    suitClass = card.clubs ? "peek-clubs" : "peek-hearts";
+  } else {
+    suitClass = "back";
+  }
+
+  $: {
+    const front = faceUp || peek;
+    if (pattern == "letters") {
+      if (front) {
+        const base = clubs ? 65 : 97;
+        text = String.fromCodePoint(base + rank - 1);
+      } else {
+        text = "X";
+      }
+    } else {
+      text = "?";
+    }
+  }
+
+  let cssClass = "";
+  let text = "";
+
+  $: {
+  }
+
+  function getCardText() {
+    const front = card.faceUp || peek;
+    if (pattern === "letters") {
+      if (front) {
+        const base = clubs ? 65 : 97;
+        return String.fromCodePoint(base + rank - 1);
+      } else {
+        return "X";
+      }
+    } else {
+      console.log("Error in 'getCardText()': invalid pattern:", pattern)
+      return undefined;
+    }
+  }
+
+  function getCardClass() {
+    let classes = ["card"]
+    if (card.faceUp) {
+        return card.clubs ? "card clib
+        classes.push(card.clubs : "clubs" : "heartw");
+    } elif (peek) {
+        classes.push(card.clubs : "peek-clibs" : "peek-hearts");
+    } else {
+        classes.push("back");
+    }
+    return classes.join(" ");
+  }
 
   //svelte.onMount(() => {
   //});
@@ -23,4 +113,7 @@ export let card;
   }
 </style>
 
+{#if false}
+{#else}
 <span class="card card-chr card-{card.color}">{card.chr}</span>
+{/if}
