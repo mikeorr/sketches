1859806756dc5ad05fc4e518395e925e8cad4f47
Mike Orr <sluggoster@gmail.com>
2021-10-24 17:56:10 -0700

    Delete unused code and add missing 'let' statements.

--- spysol/02_svelte/src/Card.svelte
+++ spysol/02_svelte/src/Card.svelte
@@ -1,24 +1,6 @@
 <script>
   import * as renderers from "./renderers.js";
 
-  const BACK = String.fromCodePoint(0x1f0a0);  // PLAYING CARD BACK character.
-
-  /*
-  const unicodeChars = [
-    // Suit 0: Clubs.
-    [0x1f0d1, 0x1f0d2, 0x1f0d3, 0x1f0d4, 0x1f0d5,
-     0x1f0d6, 0x1f0d7, 0x1f0d8, 0x1f0d8, 0x1f0da,
-     0x1f0db, 0x1fdd, 0x1fde].map(String.fromCodePoint),
-    // Suit 1: Hearts.
-    [0x1f0b1, 0x1f0b2, 0x1f0b3, 0x10fb4, 0x1f0b5,
-     0x1f0b6, 0x1f0b7, 0x1f0b8, 0x1f0b8, 0x1f0ba,
-     0x1f0bb, 0x1fbd, 0x1fbe].map(String.fromCodePoint),
-  ]
-
-  console.log("Initialized unicodeChars", unicodeChars);
-  */
-
-
   // Properties.
   export let card = {id: 0, rank: 1, suit: 0};
   export let click = null;
@@ -30,54 +12,8 @@
 
   // Other reactive variables.
   let classes;
-  let rendeerer;
-  let side;   // 1 = face up, 2 = peek, 3 = back.
-  let suitClass;
-  let text;
-
-  function getCardChar() {
-    let suitOffset, rankOffset;
-    suitOffset = card.suit ? 0x1f0b1 : 0x1f0d1;
-    if (card.rank < 12) {
-        rankOffset = card.rank - 1;
-    } else {
-      rankOffset = card.rank;
-    }
-    return String.fromCodePoint(suitOffset + rankOffset);
-  }
-
-  function getCardColor(card) {
-    return (card.suit % 2) ? "red" : "black";
-  }
-
-  function getSide(faceUp, peek) {
-    if (faceUp) {
-        return 1;
-    } else if (peek) {
-        return 2;
-    } else {
-        return 3;
-    }
-  }
-
-
-  function getClasses(card, side, selected) {
-    let classes = ["card"];
-    let c;
-    if (true) {
-        classes.push("card-chr");
-    }
-    if (true) {
-        c = "card-" + getCardColor(card);
-        if (selected) {
-            c += "-selected";
-        } else if (side == 2) {
-            c += "-peek";
-        }
-        classes.push(c);
-    }
-    return classes.join(" ");
-  }
+  let content;
+  let renderer;
 
   function  getContent(card, side) {
     if (side != 3) {
