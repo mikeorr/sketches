0ce7f266dd9445a3f3c5f40f56310b7d94bcbb84
Mike Orr <sluggoster@gmail.com>
2025-01-04 16:36:19 -0800

    Delete unused code.

--- games/spysol/03/main.js
+++ games/spysol/03/main.js
@@ -244,8 +244,6 @@ function newGame() {
 
 function init() {
     if (!initialized) {
-        const model1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-        const model2 = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
 
         DOM.models = document.getElementById("models");
         DOM.tableau = document.getElementById("tableau");
