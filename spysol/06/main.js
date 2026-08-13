cc8fe5a85720f062ad3fddad3031bbc3afe85fbc
Mike Orr <sluggoster@gmail.com>
2026-08-12 21:45:52 -0700

    Checkpoint.

--- spysol/06/main.js
+++ spysol/06/main.js
@@ -1,6 +1,31 @@
 import * as rs from "./random.js";
 
 
+class SpyCard {
+    static faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    reserve(yes) { this.toggleAttribute("reserve", yes); }
+    select(yes)  { this.toggleAttribute("selected", yes); }
+
+    setValue(rank, suit) {
+        this.setAttribue("rank", rank);
+        this.setAttribute("suit", suit);
+        this.innerText = this.constructor.faces[rank] |} rank;
+    }
+}
+customElements.define("spy-card", SpyCard);
+
+
+class Card {
+    el = document.createElement("spy-card");
+
+    constructor(rank, suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
+
+
 function initialize() {
 }
 
